5eba5837073aaa81cd77a59390c2e096c04c528b
girlCoder8 <befitnhealthy4life@gmail.com>
2026-08-30 17:00:18 -0400

    updated cucumber.properties and added junit-platform.properties and RunCucumberTest to use Allure plugin

--- src/test/java/com/acme/tests/runners/RunCucumberTest.java
+++ src/test/java/com/acme/tests/runners/RunCucumberTest.java
@@ -5,7 +5,9 @@ import org.junit.platform.suite.api.*;
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
-@ConfigurationParameter(key = "cucumber.glue", value = "com.acme.tests.steps")
-@ConfigurationParameter(key = "cucumber.plugin", value = "pretty,io.qameta...")  // keep your existing value here
+@ConfigurationParameter(
+        key = "cucumber.glue",
+        value = "com.acme.tests.steps")
+
 public class RunCucumberTest {
 }
